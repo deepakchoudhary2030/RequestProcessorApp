c9847ac6a7da824b0671794e864db29f440fe115
deepak ch <deepakchoudhary2030@gmail.com>
2026-03-24 09:40:26 +0530

    Added controller code to create the request and insert the request in Job  queue

--- request_processor_service/app/controllers/api/v1/requests_controller.rb
+++ request_processor_service/app/controllers/api/v1/requests_controller.rb
@@ -1,5 +1,20 @@
 class Api::V1::RequestsController < ApplicationController
     def create
+        request = Request.find_by(request_id: params[:request_id])
+
+        if request
+          return render json: { message: "Already exists", status: request.status }, status: :ok
+        end
+
+        request = Request.new(request_params)
+
+        if request.save
+          ProcessRequestJob.perform_later(request.request_id)
+
+          render json: { message: "Request accepted", id: request.request_id }, status: :created
+        else
+          render json: { errors: request.errors.full_messages }, status: :unprocessable_entity
+        end
     end
     
     def show
@@ -7,5 +22,11 @@ class Api::V1::RequestsController < ApplicationController
     end
 
     def cancel
+    end
+
+    private
+
+    def request_params
+        params.require(:request).permit(:request_id, :data)
     end
 end
